d672e9e1677da601f5928ad9bff23ec8af7d01be
Florian Rodriguez <florian@negative-network.com>
2021-03-29 12:29:59 +0200

    Début du cours laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('main')
+
+  <h2>Contact</h2>
+  <p>Ceci est ma page contact</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('main')
+
+  <h1>Home</h1>
+  <p>Ceci est ma page Home</p>
+
+@endsection
